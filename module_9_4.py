60885c5decaeac6937a4ed99590aa435301f30a2
louol <34587874+louol@users.noreply.github.com>
2024-11-03 14:24:00 +0800

    Lesson_9_4

---
+++ module_9_4.py
@@ -0,0 +1,32 @@
+# Lambda-функция:
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+sr = lambda x, y: x == y
+result = list(map(sr, first, second))
+print(list(result))
+
+# Замыкание:
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for item in data_set:
+                file.write(f'{item}\n')
+    return  write_everything
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+# Метод __call__:
+from random import choice
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
+
